f0be140885b5a6e2a2f43532524991d31409eccb
unknown <dasutinraioneru@gmail.com>
2023-03-31 14:47:24 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const history = document.querySelector('#history-value')
+const result = document.querySelector('#result-value')
+
+const divide = document.querySelector('#dv')
+
+console.log(divide.id);
+
+const button = document.getElementsByTagName("button")
+
+const click = () =>{
+
+}
+
+addEventListener("click", click)
